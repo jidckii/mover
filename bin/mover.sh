302edd0126fc72f2713e7a6fd45cf8de47faf942
jidckii <jidckii@gmail.com>
2016-11-14 12:14:05 +0500

    fix code style + добавил минималистичный прогресс бар

--- bin/mover.sh
+++ bin/mover.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# set -x
+
 log_daemon=/opt/mover/log/daemon.log
 
 end_dir=/home/transcoder/queue-video-tmp/
@@ -10,14 +13,22 @@ text3="видео файлов."
 text4="Введите ИМЯ для видео материала:"
 text5="Отсутствует видео в формате .MP4 или .MTS , проверьте USB носитель на PC"
 text6="Копирование завершено, извлеките USB носитель !!!"
-text7="Идет копирование, не извлекайте SD карту! ..."
+wait_copy="Идет копирование, не извлекайте SD карту! ..."
 err_enter="Вы не ввели имя, повторить поиск ?"
+err_copy="Ошибка при копировании, повторить поиск ?"
+
+to_copy(){
+      find $mediapath -name "$1" -print0 | \
+      xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ | \
+      zenity --progress --pulsate --title="Копирование" --text="$wait_copy" \
+      --auto-close --auto-kill
+}
 
 # Заходим в цикл и работаем как бы в режиме демона
 while true; do
   sleep 5
-  usbsizenum=`du -s $mediapath | awk '{print $1}'`
-  usbsizehum_num=`du -s -h $mediapath | awk '{print $1}'`
+  usbsizenum=$(du -s $mediapath | awk '{print $1}')
+  usbsizehum_num=$(du -s -h $mediapath | awk '{print $1}')
 
   # Если размер каталога меньше 1 мегабайта, начинаем цикл заново.
   if [ "$usbsizenum" -lt "1000" ]; then
@@ -25,7 +36,7 @@ while true; do
   fi
 
   echo -e '\n' "\e[0;32m $text1 \e[1;95m $usbsizehum_num \e[0m" '\n'
-  worklist=`find $mediapath -name *.MTS -o -name *.MP4 | wc -l`
+  worklist=$(find $mediapath -name *.MTS -o -name *.MP4 | wc -l)
 
   # Если файлов в формате по маске не обнаружено, начинаем цикл заново.
   if [ "$worklist" -eq "0" ]; then
@@ -42,15 +53,27 @@ while true; do
       zenity --error --title="Ошибка" --text="$err_enter"
       continue
     fi
+  fi
     dir_name=$(echo -n $dir_name | sed 's/ /_/g')
-    echo -e '\n' "\e[1;32m $text7 \e[0m" '\n'
+    echo -e '\n' "\e[1;31m Введено имя: $dir_name \e[0m" '\n'
+    echo -e '\n' "\e[1;32m $wait_copy \e[0m" '\n'
     # zenity --error --title="Инфо" --text="$text7"
 
-    find $mediapath -name "*.MP4" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ & pid1=$!
-    find $mediapath -name "*.MTS" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ & pid2=$!
-  fi
-  wait $pid1
-  wait $pid2
+    worklist=$(find $mediapath -name *.MTS -o -name *.MP4 | awk -F. '{print $NF}' | sed -n -e 1p)
+    if [ "$worklist" = "MP4" ]; then
+    to_copy "*.MP4"
+      if [[ "$?" -ne 0 ]]; then
+        zenity --error --title="Ошибка" --text="$err_copy"
+        continue
+      fi
+    elif [ "$worklist" = "MTS" ]; then
+      to_copy "*.MTS"
+      # wait $pid
+      if [[ "$?" -ne 0 ]]; then
+        zenity --error --title="Ошибка" --text="$err_copy"
+        continue
+      fi
+    fi
 
   echo -e '\n' "\e[4;32m $text6 \e[0m" '\n'
   sleep 5
@@ -59,7 +82,7 @@ while true; do
   # ждем извлечения USB.
   while [ "$usbsizeend" -eq "$usbsizenum" ]; do
     sleep 2
-    usbsizeend=`du -s $mediapath | awk '{print $1}'`
+    usbsizeend=$(du -s $mediapath | awk '{print $1}')
 
     if [ "$usbsizeend" -ne "$usbsizenum" ];  then
       echo -e '\n' "\e[1;96m $text2 \e[0m" '\n'
