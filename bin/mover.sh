f7246d338d266e8e8d5edd9a3ffc9f6e6049159a
jidckii <jidckii@gmail.com>
2016-11-14 08:32:04 +0500

    добавил ввод имени через zenity

--- bin/mover.sh
+++ bin/mover.sh
@@ -7,10 +7,11 @@ mediapath=/media
 text1="Обнаружен USB носитель, с данными объемом "
 text2="USB носитель отсутствует"
 text3="видео файлов."
-text4="Введите ИМЯ для инджестируемого материала:"
+text4="Введите ИМЯ для видео материала:"
 text5="Отсутствует видео в формате .MP4 или .MTS , проверьте USB носитель на PC"
 text6="Копирование завершено, извлеките USB носитель !!!"
 text7="Идет копирование, не извлекайте SD карту! ..."
+err_enter="Вы не ввели имя, повторить поиск ?"
 
 # Заходим в цикл и работаем как бы в режиме демона
 while true; do
@@ -34,18 +35,22 @@ while true; do
 
   if [ "$worklist" -gt "0" ]; then
     echo -e '\n' "\e[0;32m Найдено \e[1;95m $worklist \e[0;32m $text3 \e[0m" '\n'
-    echo -e '\n' "\e[1;33m $text4 \e[0m" '\n'
-    read dir_name
+    # echo -e '\n' "\e[1;33m $text4 \e[0m" '\n'
+    dir_name=$(zenity --entry --title="Ввод имени" --text="$text4")
+
+    if [ -z "$dir_name" ]; then
+      zenity --error --title="Ошибка" --text="$err_enter"
+      continue
+    fi
+
     echo -e '\n' "\e[1;32m $text7 \e[0m" '\n'
-    find $mediapath -name "*.MP4" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ &
-    find $mediapath -name "*.MTS" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ &
+    # zenity --error --title="Инфо" --text="$text7"
+
+    find $mediapath -name "*.MP4" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ & pid1=$!
+    find $mediapath -name "*.MTS" -print0 | xargs -0 -I% rsync -a % transcoder@172.20.0.10:$end_dir$dir_name/ & pid2=$!
   fi
-  sleep 5
-  ps_status=`ps auxww | grep 'xargs'| grep -v grep | wc -l`
-  while [ "$ps_status" -gt "0" ]; do
-    sleep 5
-    ps_status=`ps auxww | grep 'xargs'| grep -v grep | wc -l`
-  done
+  wait $pid1
+  wait $pid2
 
   echo -e '\n' "\e[4;32m $text6 \e[0m" '\n'
   sleep 5
